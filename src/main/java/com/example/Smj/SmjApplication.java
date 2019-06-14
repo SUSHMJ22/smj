860d772fd78685b17fa8efa63e48d3b3df08a4e0
SUSHMJ22 <jadhavsushma226@gmail.com>
2019-06-14 22:35:16 +0530

    first commit

---
+++ src/main/java/com/example/Smj/SmjApplication.java
@@ -0,0 +1,13 @@
+package com.example.Smj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmjApplication.class, args);
+	}
+
+}
